9977abe67a323d0c8da751ca3468b6f7e6530680
Engin Karatas <enginkaratas99@gmail.com>
2024-12-03 14:18:22 +0300

    create get book operations complied with api response

--- src/controller/BookController.ts
+++ src/controller/BookController.ts
@@ -6,20 +6,28 @@ export class BookController {
   private bookRepository = AppDataSource.getRepository(Book);
 
   async getAllBooks(request: Request, response: Response, next: NextFunction) {
-    return this.bookRepository.find();
+    return this.bookRepository.find({
+      select: { id: true, name: true },
+    });
   }
 
   async getBook(request: Request, response: Response, next: NextFunction) {
     const id = parseInt(request.params.id);
 
     const book = await this.bookRepository.findOne({
+      select: { id: true, name: true, userScore: true },
       where: { id },
     });
 
     if (!book) {
       return "unregistered book";
     }
-    return book;
+    const bookResponseModel = {
+      id: book.id,
+      name: book.name,
+      score: book.userScore === "-1" ? Number(book.userScore) : book.userScore,//to comply given api response
+    };
+    return bookResponseModel;
   }
 
   async borrowBook(request: Request, response: Response, next: NextFunction) {
