8f7b292c09922bcfa5a6e01889e15b8ce7ee08e1
Engin Karatas <enginkaratas99@gmail.com>
2024-12-03 17:04:15 +0300

    add mock: 3 user and 6 book

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,127 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { Request, Response } from "express"
-import { AppDataSource } from "./data-source"
-import { Routes } from "./routes"
-import { User } from "./entity/User"
-import { Book } from "./entity/Book"
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Request, Response } from "express";
+import { AppDataSource } from "./data-source";
+import { Routes } from "./routes";
+import { User } from "./entity/User";
+import { Book } from "./entity/Book";
 
-AppDataSource.initialize().then(async () => {
+AppDataSource.initialize()
+  .then(async () => {
+    const app = express();
+    app.use(bodyParser.json());
 
-    const app = express()
-    app.use(bodyParser.json())
-
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
-            }
-        })
-    })
-
-    app.listen(3000)
- 
-    const newTestUser1 = await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            name: "Test User"
-        })
-    );
-
-    const book = AppDataSource.manager.create(Book, {
-        author: "Test Author",
-        name: "Test Book",
-        publisher: "Test Publisher",
-        user: newTestUser1,
-        year: 2023,
-        lendStatus: -1,
-        score: -1
+    Routes.forEach((route) => {
+      (app as any)[route.method](
+        route.route,
+        (req: Request, res: Response, next: Function) => {
+          const result = new (route.controller as any)()[route.action](
+            req,
+            res,
+            next
+          );
+          if (result instanceof Promise) {
+            result.then((result) =>
+              result !== null && result !== undefined
+                ? res.send(result)
+                : undefined
+            );
+          } else if (result !== null && result !== undefined) {
+            res.json(result);
+          }
+        }
+      );
     });
 
-    try {
-        const savedBook = await AppDataSource.manager.save(book);
-        console.log("Book saved successfully:", savedBook);
-    } catch (error) {
-        console.error("Error saving book:", error);
-    }
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+    app.listen(3000);
 
-}).catch(error => console.log(error))
+    await AppDataSource.manager.save(
+      AppDataSource.manager.create(User, {
+        name: "Test User",
+      })
+    );
+    await AppDataSource.manager.save(
+      AppDataSource.manager.create(User, {
+        name: "Test User2",
+      })
+    );
+    await AppDataSource.manager.save(
+      AppDataSource.manager.create(User, {
+        name: "Test User3",
+      })
+    );
+    
+    const book1 = AppDataSource.manager.create(Book, {
+      author: "Test Author",
+      name: "Test Book",
+      publisher: "Test Publisher",
+      user: null,
+      year: 2023,
+      lendStatus: -1,
+      userScore: "-1",
+    });
+
+    const book2 = AppDataSource.manager.create(Book, {
+      author: "Test Author2",
+      name: "Test Book2",
+      publisher: "Test Publisher2",
+      user: null,
+      year: 2023,
+      lendStatus: -1,
+      userScore: "1",
+    });
+
+    const book3 = AppDataSource.manager.create(Book, {
+      author: "Test Author3",
+      name: "Test Book3",
+      publisher: "Test Publisher3",
+      user: null,
+      year: 2023,
+      lendStatus: -1,
+      userScore: "2",
+    });
+
+    const book4 = AppDataSource.manager.create(Book, {
+      author: "Test Author4",
+      name: "Test Book4",
+      publisher: "Test Publisher4",
+      user: null,
+      year: 2023,
+      lendStatus: -1,
+      userScore: "3",
+    });
+
+    const book5 = AppDataSource.manager.create(Book, {
+      author: "Test Author5",
+      name: "Test Book5",
+      publisher: "Test Publisher5",
+      user: null,
+      year: 2023,
+      lendStatus: -1,
+      userScore: "4",
+    });
+
+    const book6 = AppDataSource.manager.create(Book, {
+      author: "Test Author6",
+      name: "Test Book6",
+      publisher: "Test Publisher6",
+      user: null,
+      year: 2023,
+      lendStatus: -1,
+      userScore: "5",
+    });
+
+    await AppDataSource.manager.save([
+      book1,
+      book2,
+      book3,
+      book4,
+      book5,
+      book6,
+    ]);
+
+    console.log(
+      "Express server has started on port 3000. Open http://localhost:3000/users to see results"
+    );
+  })
+  .catch((error) => console.log(error));
