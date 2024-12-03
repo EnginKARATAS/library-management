5f13d21c3615d495e309aff5e8fb10d5f9a9c990
Engin Karatas <enginkaratas99@gmail.com>
2024-12-03 20:03:56 +0300

    add route

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -1,10 +1,22 @@
-import './App.css'
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import "./App.scss";
+import Footer from "./components/layout/Footer";
+import MainPage from "./pages/MainPage";
+import BooksPage from "./pages/BooksPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/books" element={<BooksPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
