8f7cc7166e2b1f6e11d3f29f2bad69021174560e
Engin Karatas <enginkaratas99@gmail.com>
2024-12-03 23:21:43 +0300

    create responsive main page

--- library-frontend/src/pages/MainPage.tsx
+++ library-frontend/src/pages/MainPage.tsx
@@ -1,9 +1,53 @@
+import { Grid2 as Grid } from "@mui/material";
 import "../styles/components/mainPage.scss";
-
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 export default function MainPage() {
+  const navigate = useNavigate();
+  const onButtonClick = () => {
+    navigate("/books");
+  };
+  
   return (
-    <div className="main-page-container">
-      <h1> Awesome Books</h1>
-    </div>
+    <Grid
+      container
+      justifyContent="center"
+      className="main-page-container"
+    >
+      <div className="first-section-container">
+        <Grid container>
+          <Grid justifyContent="center" alignItems="center">
+            <Grid container justifyContent="center" alignItems="center" className="left-side">
+              <img
+                style={{ width: "92%" }}
+                src="/firstSectionImage1.png"
+                alt="first section left image"
+              />
+            </Grid>
+            <Grid
+              className="button-side"
+              container
+              justifyContent="center"
+              alignItems="center"
+              direction="column"
+            >
+              <Button onClick={()=>onButtonClick()} variant="contained" style={{ backgroundColor: "#4a2e6f", color: "white" }}>Book Management</Button>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            className="right-side"
+          >
+            <img
+              style={{ width: "92%" }}
+              src="/firstSectionImage2.png"
+              alt="first section right image"
+            />
+          </Grid>
+        </Grid>
+      </div>
+    </Grid>
   );
 }
