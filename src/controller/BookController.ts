747ded67b2893aeec1581416b232d27643396ac3
Engin Karatas <enginkaratas99@gmail.com>
2024-12-03 17:13:59 +0300

    add the borrow and the return book route handlers

--- src/controller/BookController.ts
+++ src/controller/BookController.ts
@@ -25,16 +25,72 @@ export class BookController {
     const bookResponseModel = {
       id: book.id,
       name: book.name,
-      score: book.userScore === "-1" ? Number(book.userScore) : book.userScore,//to comply given api response
+      score: book.userScore === "-1" ? Number(book.userScore) : book.userScore, //to comply given api response
     };
     return bookResponseModel;
   }
 
   async borrowBook(request: Request, response: Response, next: NextFunction) {
-    return this.bookRepository.save(null);
+    try {
+      const userId = parseInt(request.params.id);
+      const bookId = parseInt(request.params.bookId);
+
+      const borrowRequestBook = await this.bookRepository.findOne({
+        where: { id: bookId },
+        relations: ["user"],
+      });
+      if (borrowRequestBook?.user) {
+        response.status(404).send("Book already borrowed");
+        return;
+      }
+
+      const updated = await this.bookRepository.update(bookId, {
+        user: { id: userId },
+        lendStatus: 0,
+      });
+
+      if (updated.affected) {
+        response.status(204).send();
+        return;
+      }
+      response.status(404).send();
+      return;
+    } catch (error) {
+      console.log(error);
+      response.status(500).send();
+      return;
+    }
   }
 
   async returnBook(request: Request, response: Response, next: NextFunction) {
-    return this.bookRepository.save(null);
+    try {
+      const userId = parseInt(request.params.id);
+      const bookId = parseInt(request.params.bookId);
+
+      const existingBook = await this.bookRepository.findOne({
+        where: { id: bookId, user: { id: userId } },
+      });
+      if (!existingBook) {
+        response.status(404).send();
+        return;
+      }
+      const updated = await this.bookRepository.update(bookId, {
+        user: { id: null },
+        lendStatus: -1,
+      });
+
+      await this.bookRepository.save({ ...existingBook, lendStatus: 1 });
+
+      if (updated.affected) {
+        response.status(204).send();
+        return;
+      }
+
+      return;
+    } catch (error) {
+      console.log(error);
+      response.status(500).send();
+      return;
+    }
   }
 }
