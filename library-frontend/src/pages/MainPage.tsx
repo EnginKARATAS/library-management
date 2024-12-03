b20f99e062bebea3a5b72c2e201ba50559a7c813
Engin Karatas <enginkaratas99@gmail.com>
2024-12-03 20:01:07 +0300

    add pages

---
+++ library-frontend/src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import "../styles/components/mainPage.scss";
+
+export default function MainPage() {
+  return (
+    <div className="main-page-container">
+      <h1> Awesome Books</h1>
+    </div>
+  );
+}
