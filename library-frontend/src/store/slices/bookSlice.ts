780d59378f705a948d2804f001894f9eaa43e588
Engin Karatas <enginkaratas99@gmail.com>
2024-12-04 20:25:52 +0300

    update book type

--- library-frontend/src/store/slices/bookSlice.ts
+++ library-frontend/src/store/slices/bookSlice.ts
@@ -1,14 +1,20 @@
-import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
 
- 
 type Book = {
   id: number;
   name: string;
   score: string;
-}
+};
 
-type OneBook = Omit<Book, 'score'>;
+type OneBook = Omit<Book, "score"> & {
+  id: number;
+  name: string;
+  score?: number;
+  author?: string;
+  year?: string;
+  publisher?: string;
+};
 
 interface BookState {
   books: Book[] | null;
@@ -24,25 +30,30 @@ const initialState: BookState = {
   error: null,
 };
 
-export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
-  const response = await axios.get('http://localhost:3000/books');
-  return response.data;
-});
+export const fetchBooks = createAsyncThunk<Book[]>(
+  "books/fetchBooks",
+  async () => {
+    const response = await axios.get("http://localhost:3000/books");
+    return response.data;
+  }
+);
 
 export const fetchBookDetails = createAsyncThunk(
-  'books/fetchBookDetails',
+  "books/fetchBookDetails",
   async (bookId: number) => {
+    if (!bookId && bookId !== 0) {
+      throw new Error('Book ID is required');
+    }
     const response = await axios.get(`http://localhost:3000/books/${bookId}`);
+    console.log(response.data);
     return response.data;
   }
 );
 
 const bookSlice = createSlice({
-  name: 'book',
+  name: "book",
   initialState,
-  reducers: {
-
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchBooks.pending, (state) => {
@@ -55,19 +66,22 @@ const bookSlice = createSlice({
       })
       .addCase(fetchBooks.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.error.message || 'Failed to fetch users';
+        state.error = action.error.message || "Failed to fetch users";
       })
       .addCase(fetchBookDetails.pending, (state) => {
         state.loading = true;
         state.error = null;
       })
-      .addCase(fetchBookDetails.fulfilled, (state, action: PayloadAction<OneBook>) => {
-        state.loading = false;
-        state.oneBook = action.payload;
-      })
+      .addCase(
+        fetchBookDetails.fulfilled,
+        (state, action: PayloadAction<OneBook>) => {
+          state.loading = false;
+          state.oneBook = action.payload;
+        }
+      )
       .addCase(fetchBookDetails.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.error.message || 'Failed to fetch book details';
+        state.error = action.error.message || "Failed to fetch book details";
       });
   },
 });
