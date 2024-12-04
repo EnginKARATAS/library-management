138c68893a9b248046ea1dbadc440d6b2c431bde
Engin Karatas <enginkaratas99@gmail.com>
2024-12-04 15:17:33 +0300

    fill users page

--- library-frontend/src/pages/UsersPage.tsx
+++ library-frontend/src/pages/UsersPage.tsx
@@ -1,3 +1,74 @@
+import { Button } from "@mui/material";
+import { Grid2 as Grid } from "@mui/material";
+import "../styles/components/booksPage.scss";
+//import { useNavigate } from "react-router-dom";
+import {
+  useDialogs,
+} from "@toolpad/core/useDialogs";
+import BasePopup from "../components/popup/BasePopup";
+import BookDetail from "../components/popup/BookDetail";
 export default function UsersPage() {
-    return <div>Users Page</div>;
+  //const navigate = useNavigate();
+  const users = [
+    { id: 1, name: "Engin Karataş" },
+    { id: 2, name: "Test User" },
+  ];
+
+  const dialogs = useDialogs();
+
+  const onBookDetailsClick = async (id: number) => {
+    const bookId = await dialogs.open(BasePopup, {
+      component: <BookDetail />,
+      title: "Book Details",
+    });
+    
+    if (bookId) {
+      dialogs.alert(`The book was sended to user with ID: ${bookId}`, {
+        title: "Success",
+      });
+    }
+  };
+  return (
+    <Grid
+      className="main-container"
+      container
+      justifyContent="center"
+      alignItems="center"
+    >
+      <div className="operaions-container">
+        <h1>Users List</h1>
+        <Grid
+          container
+          className="books-page-row books-page-header"
+          justifyContent="space-between"
+        >
+          <Grid>#</Grid>
+          <Grid>User Name</Grid>
+          <Grid>User Detail</Grid>
+        </Grid>
+        {users.map((book, index) => (
+          <Grid
+            container
+            key={book.id}
+            justifyContent="space-between"
+            className="books-page-row"
+          >
+            <Grid textAlign="center">{index + 1}</Grid>
+            <Grid textAlign="left">{book.name}</Grid>
+            <Grid textAlign="center">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  onBookDetailsClick(book.id);
+                }}
+              >
+                Details
+              </Button>
+            </Grid>
+          </Grid>
+        ))}
+      </div>
+    </Grid>
+  );
 }
