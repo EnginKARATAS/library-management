f125ca85adc77b2491bf2d0ceeba57e3c6733a43
Engin Karatas <enginkaratas99@gmail.com>
2024-12-04 14:12:40 +0300

    add base and userDetail popup

---
+++ library-frontend/src/components/popup/BasePopup.tsx
@@ -0,0 +1,39 @@
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+import { DialogProps } from "@toolpad/core";
+import LoadingButton from "@mui/lab/LoadingButton";
+import * as React from "react";
+
+export default function BasePopup({
+  open,
+  onClose,
+  payload,
+}: DialogProps<{ component: React.ReactNode; title: string }, string | null>) {
+  const [loading, setLoading] = React.useState(false);
+
+  return (
+    <Dialog fullWidth open={open} onClose={() => onClose(null)}>
+      <DialogTitle>{payload.title}</DialogTitle>
+      <DialogContent>{payload.component}</DialogContent>
+      <DialogActions>
+        <LoadingButton
+          loading={loading}
+          onClick={async () => {
+            setLoading(true);
+            try {
+              onClose("uuid");
+            } finally {
+              setLoading(false);
+            }
+          }}
+        >
+          Submit
+        </LoadingButton>
+      </DialogActions>
+    </Dialog>
+  );
+}
