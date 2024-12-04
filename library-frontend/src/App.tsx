a43c53326ed82a6e654a44639d49885125673431
Engin Karatas <enginkaratas99@gmail.com>
2024-12-04 15:11:44 +0300

    add empty user page

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -5,14 +5,18 @@ import BooksPage from "./pages/BooksPage";
 import AboutPage from "./pages/AboutPage";
 import { Routes, Route } from "react-router-dom";
 import "./App.scss";
+import Header from "./components/layout/Header";
+import UsersPage from "./pages/UsersPage";
 
 function App() {
   return (
       <DialogsProvider>
+        <Header />
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/books" element={<BooksPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/users" element={<UsersPage />} />
         </Routes>
       </DialogsProvider>
   );
